d2b36cfc3700e9d7c562d70788509d7f042a8895
workcantiere <worknicoletta434@gmail.com>
2026-05-21 10:59:59 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.FAS3_API_URL = "https://script.google.com/macros/s/AKfycbzHzWbvEFUbZQLBuJVWKNPYqDEJp-0DxuofxiFCPS3f7IZEoWU4UnD_fyMoNW4PNX2nbQ/exec";
+window.FAS3_API_URL = "https://script.google.com/macros/s/AKfycbx3zXb1TMZMU_uk3QaRKLpKDpUnOxfSaZJ03DHhszk5wRqw1VvucsDk8wjaK4XD8qOSfw/exec";
